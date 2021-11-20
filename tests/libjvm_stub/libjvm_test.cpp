69eb22da855a69cf2eb7963798ac509501f9e745
Fabian Meumertzheim <fabian@meumertzhe.im>
2021-11-20 08:25:26 +0100

    Work around repo mapping issue in runfiles discovery

--- tests/libjvm_stub/libjvm_test.cpp
+++ tests/libjvm_stub/libjvm_test.cpp
@@ -25,7 +25,13 @@
 
 #include "tools/cpp/runfiles/runfiles.h"
 
+#define HELLO_FROM_JAVA_JAR_PATH "libjvm_stub/HelloFromJava_deploy.jar"
 #define HELLO_FROM_JAVA_MSG "Hello_from_Java!"
+#ifdef _WIN32
+#define CLASSPATH_SEPARATOR ";"
+#else
+#define CLASSPATH_SEPARATOR ":"
+#endif
 
 void clear_env(const char* name) {
 #ifdef _WIN32
@@ -73,10 +79,17 @@ int main(int argc, const char** argv) {
 
   // Configure the JVM by adding the test JAR to the classpath and passing a
   // message via a property.
-  std::string class_path =
-      "-Djava.class.path=" + runfiles->Rlocation(
-                                 "fmeum_rules_jni_tests/libjvm_stub/"
-                                 "HelloFromJava_deploy.jar");
+  // TODO: Since the name of the current repository differs when the tests are
+  //       loaded as a module extension, we pass in multiple paths of which only
+  //       ever one will be valid. Use rules_runfiles instead once it becomes
+  //       available as a Bazel module.
+  std::string jar_path_workspace =
+      runfiles->Rlocation("fmeum_rules_jni_tests/" HELLO_FROM_JAVA_JAR_PATH);
+  std::string jar_path_module = runfiles->Rlocation(
+      ".install_dev_dependencies.fmeum_rules_jni_"
+      "tests/" HELLO_FROM_JAVA_JAR_PATH);
+  std::string class_path = "-Djava.class.path=" + jar_path_workspace +
+                           CLASSPATH_SEPARATOR + jar_path_module;
   JavaVM* jvm = nullptr;
   JNIEnv* env = nullptr;
   JavaVMInitArgs vm_args;
