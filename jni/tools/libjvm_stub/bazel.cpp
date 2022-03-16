5fdd461a0df4ac89dc8b5e3c90ad0106ce593807
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-03-16 16:24:49 +0100

    Add coverage support for libjvm_stub

--- jni/tools/libjvm_stub/bazel.cpp
+++ jni/tools/libjvm_stub/bazel.cpp
@@ -23,7 +23,10 @@
 #include "rules_jni_internal.h"
 #include "tools/cpp/runfiles/runfiles.h"
 
+#define JACOCO_MAIN_CLASS "JACOCO_MAIN_CLASS"
+#define JAVA_TOOL_OPTIONS "JAVA_TOOL_OPTIONS"
 #define MSG_PREFIX "[rules_jni]: "
+#define RULES_JNI_COVERAGE_AGENT_JAR "RULES_JNI_COVERAGE_AGENT_JAR"
 
 using ::bazel::tools::cpp::runfiles::Runfiles;
 
@@ -37,6 +40,14 @@ static Runfiles* get_runfiles() {
   return Runfiles::Create(rules_jni_arg0);
 }
 
+static void set_env(const std::string& key, const std::string& value) {
+#ifdef _WIN32
+  _putenv_s(key.c_str(), value.c_str());
+#else
+  setenv(key.c_str(), value.c_str(), 1);
+#endif
+}
+
 void rules_jni_init(const char* argv0) {
   rules_jni_arg0 = argv0;
   Runfiles* runfiles = get_runfiles();
@@ -46,12 +57,54 @@ void rules_jni_init(const char* argv0) {
     // Setting the runfiles variables for the current process is the only way
     // to get them to the Java runfiles library.
     for (const std::pair<std::string, std::string>& envvar : envvars) {
-#ifdef _WIN32
-      _putenv_s(envvar.first.c_str(), envvar.second.c_str());
-#else
-      setenv(envvar.first.c_str(), envvar.second.c_str(), 1);
-#endif
+      set_env(envvar.first, envvar.second);
     }
+  }
+  if (RULES_JNI_COLLECT_COVERAGE) {
+    std::string coverage_agent;
+    if (runfiles != nullptr) {
+      coverage_agent = runfiles->Rlocation(
+          "fmeum_rules_jni/jni/tools/libjvm_stub/coverage/"
+          "CoverageAgent_deploy.jar");
+    } else if (getenv(RULES_JNI_COVERAGE_AGENT_JAR) != nullptr) {
+      // This should only every happen in tests.
+      coverage_agent = getenv(RULES_JNI_COVERAGE_AGENT_JAR);
+    }
+    if (coverage_agent.empty()) {
+      fprintf(stderr, MSG_PREFIX "failed to find CoverageAgent");
+      exit(EXIT_FAILURE);
+    }
+    // The JVM prepends JAVA_TOOL_OPTIONS to the options passed to the
+    // CreateJavaVM call. By prepending our agent to the variable, we ensure
+    // that the coverage setup code runs as early as possible. The agent is
+    // essentially just a startup hook and does not perform any logic other
+    // than executing Bazel's JacocoCoverageRunner's main function.
+    // Note: The value of JAVA_TOOL_OPTIONS may be changed by the program
+    // between the call to rules_jni_init and the call to CreateJavaVM. If it
+    // is overwritten, this is arguably a bug in the program and if it is not,
+    // coverage will still be collected. This seems better than the
+    // alternative of modifying the environment right before the stub makes
+    // the actual call to CreateJavaVM as setenv on Linux is inherently unsafe
+    // to call after the very early, synchronous setup stage of a program.
+    std::string java_agent_option = "'-javaagent:" + coverage_agent + "'";
+    const char* java_tool_options = getenv(JAVA_TOOL_OPTIONS);
+    if (java_tool_options == nullptr) {
+      set_env(JAVA_TOOL_OPTIONS, java_agent_option);
+    } else {
+      set_env(JAVA_TOOL_OPTIONS, java_agent_option + " " + java_tool_options);
+    }
+    // We have to ensure that JacocoCoverageRunner#getMainClass always returns
+    // our empty main method as CreateJavaVM is not expected to actually run
+    // any main classes. There are two cases:
+    // 1. Our agent deploy jar is the only jar on the classpath. In this case,
+    // the insideDeployJar parameter may be true, but since we do not define
+    // the Coverage-Main-Class attribute in the agent's manifest, the function
+    // will return the value of JACOCO_MAIN_CLASS set below.
+    // 2. Our agent is not the only jar on the classpath. In this case,
+    // metadataFiles in JacocoCoverageRunner#main will have length at least 2
+    // and thus insideDeployJar will always be false.
+    set_env(JACOCO_MAIN_CLASS,
+            "com.github.fmeum.rules_jni.libjvm_stub.CoverageAgent");
   }
 }
 
---
+++ jni/tools/libjvm_stub/coverage/src/main/java/com/github/fmeum/rules_jni/libjvm_stub/CoverageAgent.java
@@ -0,0 +1,60 @@
+// Copyright 2022 Fabian Meumertzheim
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.github.fmeum.rules_jni.libjvm_stub;
+
+import com.google.testing.coverage.JacocoCoverageRunner;
+import java.io.File;
+import java.io.IOException;
+import java.lang.instrument.Instrumentation;
+import java.net.URISyntaxException;
+import java.util.jar.JarFile;
+
+public final class CoverageAgent {
+  public static void premain(String args, Instrumentation instrumentation) {
+    appendSelfToBootstrapClassLoaderSearch(instrumentation);
+    try {
+      JacocoCoverageRunner.main(new String[] {});
+    } catch (Exception e) {
+      error("Failed to collect coverage in JVM created via JNI_CreateJavaVM:", e);
+    }
+  }
+
+  public static void main(String[] args) {
+    // No-op, JVMs created via the Java Invocation API do not automatically execute the main method.
+  }
+
+  private static void appendSelfToBootstrapClassLoaderSearch(Instrumentation instrumentation) {
+    try {
+      JarFile ownJar = new JarFile(new File(
+          CoverageAgent.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
+      instrumentation.appendToBootstrapClassLoaderSearch(ownJar);
+    } catch (IOException | NullPointerException | URISyntaxException e) {
+      warn(
+          "Failed to append JaCoCo to bootstrap class loader search path, which may cause test failures",
+          e);
+    }
+  }
+
+  private static void error(String message, Throwable cause) {
+    System.err.println("[rules_jni] " + message + ":");
+    cause.printStackTrace();
+    Runtime.getRuntime().halt(1);
+  }
+
+  private static void warn(String message, Throwable cause) {
+    System.err.println("[rules_jni] " + message + ":");
+    cause.printStackTrace();
+  }
+}
